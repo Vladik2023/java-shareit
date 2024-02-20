e6395a1d4471fedbce83afa204f1a8b8fd403330
Vlad <vladbabaev0128@yandex.ru>
2024-02-20 20:25:31 +0300

    1 версия проекта

---
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.item.dto;
+
+
+import ru.practicum.shareit.item.model.Item;
+
+public class ItemMapper {
+    public static ItemDto convertToDto(Item item) {
+        return new ItemDto(item.getId(), item.getName(), item.getDescription(), item.isAvailableToRent());
+    }
+
+    public static Item convertToEntity(ItemDto itemDto, long ownerId) {
+        return new Item(itemDto.getId(), itemDto.getName(), itemDto.getDescription(), itemDto.getIsAvailableToRent(),
+                ownerId);
+    }
+}
